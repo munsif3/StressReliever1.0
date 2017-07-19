8bb3dec79c7e5ceeab514a9d92df6785ec21f5ab
Munsif Musthafa <immsf5@gmail.com>
2017-07-19 20:57:24 +0530

    Working application - for key down

---
+++ MushroomMove/MoveObject.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MushroomMove
+{
+    partial class MoveObject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrMushroom = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrMushroom
+            // 
+            this.tmrMushroom.Enabled = true;
+            this.tmrMushroom.Tick += new System.EventHandler(this.tmrMushroom_Tick);
+            // 
+            // MoveObject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.Name = "MoveObject";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mushroom";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MoveObject_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MoveObject_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrMushroom;
+    }
+}
+
---
+++ MushroomMove/MoveObject.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MushroomMove
+{
+    public partial class MoveObject : Form
+    {
+        enum Position
+        {
+            Left, Right, Up, Down
+        }
+
+        private int x;
+        private int y;
+        private Position objPos;
+
+        public MoveObject()
+        {
+            InitializeComponent();
+
+            x = 50;
+            y = 50;
+            objPos = Position.Down;
+        }
+
+        private void MoveObject_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.FillRectangle(Brushes.RoyalBlue, x, y, 64, 64);
+            e.Graphics.DrawImage(new Bitmap("Mushroom.png"), x, y, 64, 64);
+        }
+
+        private void tmrMushroom_Tick(object sender, EventArgs e)
+        {
+            if (objPos == Position.Right)
+            {
+                x += 5;
+            }
+            else if (objPos == Position.Left)
+            {
+                x -= 5;
+            }
+            else if (objPos == Position.Up)
+            {
+                y -= 5;
+            }
+            else if (objPos == Position.Down)
+            {
+                y += 5;
+            }
+            
+            Invalidate();
+        }
+
+        private void MoveObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                objPos = Position.Left;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                objPos = Position.Right;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                objPos = Position.Up;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                objPos = Position.Down;
+            }
+        }
+    }
+}
